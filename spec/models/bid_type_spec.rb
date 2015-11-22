8511b5c5c9172228d07bb5327ca95d0fc386c6d6
Kyle Martin <KyeMartn901@gmail.com>
2015-11-23 10:02:29 +1100

    Add BidType model and seed
    Created the BidType model and added a rake task to pre load the database with
    the default bid types.
    
    This closes #4

---
+++ spec/models/bid_type_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe BidType do
+  describe "attributes" do
+    it { is_expected.to have_attribute :name }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_uniqueness_of :name }
+
+    it { is_expected.to validate_presence_of :name }
+  end
+end
